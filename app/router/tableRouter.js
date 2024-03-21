4ede08df2b99db6f6450fbc87acc9aea6af79015
Kannerit <kanneritg@gmail.com>
2024-03-21 21:10:47 +0100

    added some notes to the first commit

--- app/router/tableRouter.js
+++ app/router/tableRouter.js
@@ -6,6 +6,7 @@ router.get("/", (req, res) => {
   User.find()
     .then((users) => {
       console.log(users);
+      // res.json(users) here if I do this I do get the proper display of all the users on the /main page but once I connect it then I am unable to display anything :( ...)
       res.render("users", { users });
     })
     .catch((err) => {
